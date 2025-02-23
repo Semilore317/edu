b0cdb40b350d0b2d201c19555e5e24f1db914a5a
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-02-23 10:23:02 +0100

    ...added a program component

--- edu/src/components/hero/Hero.jsx
+++ edu/src/components/hero/Hero.jsx
@@ -1,9 +1,15 @@
 import "./Hero.scss"
+import Aos from "aos"
+import "aos/dist/aos.css"
+import {useEffect} from "react";
 
 const Hero = () => {
+    useEffect(() => {
+        Aos.init({duration: 1000})
+    })
     return (
         <div className="hero">
-            <div className="hero__container">
+            <div className="hero__container" data-aos="fade-up">
                 <h1>We Ensure Better Education <br/> for a better world</h1>
                 <p>Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and <br/>
                     experiences needed to excel in the dynamic field of education</p>
--- edu/src/components/program/Program.jsx
+++ edu/src/components/program/Program.jsx
@@ -1,9 +1,34 @@
 import "./Program.scss"
-
+import Aos from 'aos'
+import "aos/dist/aos.css"
+import {useEffect} from "react";
 const Program = () => {
+    useEffect(() => {
+        Aos.init({duration: 800});
+    })
     return (
         <section className="program">
-            Program
+            <h3>OUR PROGRAM</h3>
+            <h1>What We Offer</h1>
+
+            <div className="program__container">
+                <div className="div__img_1">
+                    <img src="src/assets/program-1.png" alt=""/>
+                </div>
+                <div className="div__img_2">
+                    <img src="src/assets/program-2.png" alt=""/>
+                </div>
+                <div className="div__img_3">
+                    <img src="src/assets/program-3.png" alt=""/>
+                </div>
+            </div>
+
+            <div className="program__right_container">
+                <div>
+
+                </div>
+            </div>
+
         </section>
     )
 }
