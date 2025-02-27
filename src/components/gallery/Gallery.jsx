0fededd98a248ebba02e7f34b28265c1738b962e
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-02-27 21:16:29 +0100

    updated import statements for gallery images

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,7 +1,11 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import "./Gallery.scss";
 import Aos from "aos";
 import "aos/dist/aos.css";
+import gallery1 from "../../assets/gallery-1.png";
+import gallery2 from "../../assets/gallery-2.png";
+import gallery3 from "../../assets/gallery-3.png";
+import gallery4 from "../../assets/gallery-4.png";
 
 const Gallery =() => {
     useEffect(() => {
@@ -14,10 +18,10 @@ const Gallery =() => {
             <h3>Campus Photos</h3>
 
             <div className="gallery__photo_div">
-                <img src="src/assets/gallery-1.png" alt="Gallery Image 1"/>
-                <img src="src/assets/gallery-2.png" alt="Gallery Image 2"/>
-                <img src="src/assets/gallery-3.png" alt="Gallery Image 3"/>
-                <img src="src/assets/gallery-4.png" alt="Gallery Image 4"/>
+                <img src={gallery1} alt="Gallery Image 1"/>
+                <img src={gallery2} alt="Gallery Image 2"/>
+                <img src={gallery3} alt="Gallery Image 3"/>
+                <img src={gallery4} alt="Gallery Image 4"/>
             </div>
 
             <div className="gallery__btn">
